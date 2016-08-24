9e18e7a7a775e10f214aa7658f36faa0312fe836
Paul Allen <pea1@cornell.edu>
2016-08-24 13:46:30 -0400

    migrate RDS functionality to cucloud_ruby

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'mysql2', '~> 0.4.4'
 gem 'aws-sdk', '~> 2.5', '>= 2.5.5'
+# gem 'cucloud', github: "CU-CloudCollab/cucloud_ruby", branch: "add-rds-utilities"
+gem 'cucloud', github: "pauleallen/cucloud_ruby", branch: "master"
--- lock-and-snap.rb
+++ lock-and-snap.rb
@@ -1,17 +1,16 @@
 #!/usr/bin/env ruby
 
-require 'mysql2'
+require 'cucloud'
 require_relative 'mysql-util'
-require_relative 'rds-snap-util'
 
-db_host = ENV["DB_HOST"]
-db_user = ENV["DB_USER"]
-db_password = ENV["DB_PASSWORD"]
-rds_id = ENV["DB_RDS_ID"]
+db_host = ENV['DB_HOST']
+db_user = ENV['DB_USER']
+db_password = ENV['DB_PASSWORD']
+rds_id = ENV['DB_RDS_ID']
 
 if (db_host.nil? || db_user.nil? || db_password.nil? || rds_id.nil?) then
-  abort("Expecting the following environment variables to be set: " \
-        "DB_HOST, DB_USER, DB_PASSWORD, DB_RDS_ID")
+  abort('Expecting the following environment variables to be set: ' \
+        'DB_HOST, DB_USER, DB_PASSWORD, DB_RDS_ID')
 end
 
 mysql2_client = get_mysql2_client(db_host, db_user, db_password)
@@ -19,19 +18,22 @@ mysql2_client = get_mysql2_client(db_host, db_user, db_password)
 tables = get_myisam_tables(mysql2_client)
 puts "Number of target MyISAM tables: #{tables.size}"
 
-puts "Locking tables."
+puts 'Locking tables.'
 flush_and_lock_tables(mysql2_client, tables)
 
 n = get_number_locked_tables(mysql2_client)
 
-snap = create_snapshot(rds_id)
+puts 'Creating snapshot, and waiting for completion.'
+rds_utils = Cucloud::RdsUtils.new
+rds_instance = rds_utils.get_instance(rds_id)
+snap = rds_utils.create_snapshot_and_wait_until_available(rds_instance)
 
-puts "Unlocking tables."
+puts 'Unlocking tables.'
 unlock_tables (mysql2_client)
 
 mysql2_client.close
 
-abort("Unable to create snapshot.") if (snap.nil?)
+abort('Unable to create snapshot.') if (snap.nil?)
 
 exit
 
